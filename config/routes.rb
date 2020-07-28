e6be2efe76667e0aba7545d0615ea02dd9a9c3cb
Bijoy Sijo <bijoysijo21@gmail.com>
2020-07-28 14:23:18 +0530

    add devise scope for users authenticated and unauthenticated root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  resources :posts
   devise_for :users
+  devise_scope :user do
+    unauthenticated do
+      root 'posts#index', as: :unauthenticated_root
+    end
 
-  root "pages#index"
+    authenticated :user do
+      root 'posts#index', as: :authenticated_root
+    end
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  get '/about', to: 'pages#about', as: 'about'
+  resources :posts
 end
