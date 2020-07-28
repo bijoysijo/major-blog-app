56cd51458f3070f09e0edbd955079faf8a87f8a6
Bijoy Sijo <bijoysijo21@gmail.com>
2020-07-28 14:24:20 +0530

    cleared seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email:"example@gmail.com",
-	password:"12345",
-	password_confirmation:"12345",
-	admin: true)
